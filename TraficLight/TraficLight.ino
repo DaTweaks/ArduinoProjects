847dd17ba772ccf1687f7a12a92a1b94a7048af0
DaTweaks <51744514+DaTweaks@users.noreply.github.com>
2023-04-12 15:39:14 +0200

    button now works.

---
+++ TraficLight/TraficLight.ino
@@ -0,0 +1,61 @@
+int i = 0;
+
+enum inputPins{
+  buttonPin = 7
+};
+
+// Input button bools
+bool button = false;
+
+enum outputPins{
+  redTrafficlightPin = 2,
+  yellowTrafficlightPin = 3,
+  greenTrafficlightPin = 4,
+  redPedestrianlightPin = 5,
+  greenPedestrianlightPin = 6
+};
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+  // setup output pins
+  pinMode(redTrafficlightPin, OUTPUT);
+  pinMode(yellowTrafficlightPin, OUTPUT);
+  pinMode(greenTrafficlightPin, OUTPUT);
+  pinMode(redPedestrianlightPin, OUTPUT);
+  pinMode(greenPedestrianlightPin, OUTPUT);
+
+  // setup input pins
+  pinMode(buttonPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(buttonPin), buttonInterrupt, FALLING);
+
+  // for debug purposes
+  digitalWrite(redTrafficlightPin, HIGH);
+  digitalWrite(yellowTrafficlightPin, HIGH);
+  digitalWrite(greenTrafficlightPin, HIGH);
+
+  digitalWrite(redPedestrianlightPin, HIGH);
+  digitalWrite(greenPedestrianlightPin, HIGH);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  // because interrupts don't allow 
+  if(button){
+    buttonPressed();
+    button = false;
+  }
+}
+
+void buttonInterrupt(){
+  button = true;
+}
+
+void buttonPressed(){
+  delay(1000);
+  Serial.print("LOL! ");
+  Serial.print(i++);
+  Serial.println();
+}
